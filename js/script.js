976954b6989512108010f361b320f1d4485d4ca5
jihyekwak <jihye8653@gmail.com>
2021-11-30 18:22:13 -0800

    get current time function

--- js/script.js
+++ js/script.js
@@ -84,4 +84,17 @@ function currencyConverter(fromCurrency, toCurrency, amount) {
     })
 }
 
-
+// getTime
+function getTime() {
+  const time = new Date();
+  const year = time.getFullYear();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  
+  const currentDateTime = document.querySelector(".currentDateTime");
+  currentDateTime.innerHTML = `${year}/${month+1}/${date} ${hour}:${minute}`;
+}
+getTime();
+setInterval(getTime, 60000);
